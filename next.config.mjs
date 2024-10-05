fa807f5621be12ffa86d528b76fa6e097e809096
Sreenath Kalarikkal Sreekumar <sreenath.sreekumar@accenture.com>
2024-10-05 14:58:50 +0300

    Scroll behaviour updated 012

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,20 @@ import path from 'path';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // React Strict Mode (optional)
+    reactStrictMode: true,
+
+    // Webpack configuration
     webpack: (config) => {
         config.module.rules.push({
             test: /\.svg$/,
             use: ['@svgr/webpack'],
         });
+
         return config;
     },
+
+    // SASS options
     sassOptions: {
         includePaths: [path.join(process.cwd(), 'styles')],
     },
