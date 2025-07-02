4755ab5652102a622d0aa8e1751497aa7cba6d35
Lakshitha <lakshitha.k@bluebot.in>
2025-07-02 12:24:01 +0530

    GTM update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,14 +23,14 @@ export const metadata: Metadata = {
   // canonical: "https://www.artificiallabs.in",
   // image: "https://www.artificiallabs.in/artificial-logo.png",
   openGraph: {
-    title: "Artificial Labs",                   
-    description: "Generative AI based creative powerhouse",   
-    url: "https://www.artificiallabs.in",        
+    title: "Artificial Labs",
+    description: "Generative AI based creative powerhouse",
+    url: "https://www.artificiallabs.in",
     images: [
       {
-        url: "https://www.artificiallabs.in/artificial-thumbnail.png", 
-        width: 1200,                               
-        height: 630,                                
+        url: "https://www.artificiallabs.in/artificial-thumbnail.png",
+        width: 1200,
+        height: 630,
       }
     ]
   },
@@ -46,7 +46,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MRL4X2MX');
+          `
+        }} />
+        {/* End Google Tag Manager */}
+      </head>
       <body className={poppins.variable}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MRL4X2MX"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
         <Footer />
         <script defer src="https://player.vimeo.com/api/player.js" />
