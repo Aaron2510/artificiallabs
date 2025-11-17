9d5c93126d022b2a135ab96be39d67ca5369ac34
Lakshitha <lakshitha.k@bluebot.in>
2025-11-17 16:30:48 +0530

    in the news section update

--- src/app/components/InTheNews/index.tsx
+++ src/app/components/InTheNews/index.tsx
@@ -14,18 +14,24 @@ const newsItems: NewsItem[] = [
   {
     imageUrl: '/Afaqs-logo.png',
     alt: 'News 1',
+    bottomText: 'EMA Group acquires 20% stake in Bluebot Digital/Artificial Labs',
+    bottomLink: 'https://www.afaqs.com/industry-briefs/ema-group-acquires-20-stake-in-bluebot-digitalartificial-labs-10620793',
+  },
+  {
+    imageUrl: '/Afaqs-logo.png',
+    alt: 'News 2',
     bottomText: 'The author challenges the fear around Generative AI, suggesting it’s not a threat but a tool that democratises creativity like never before.',
     bottomLink: 'https://www.afaqs.com/agency-briefs/creativity-has-survived-every-tech-shift-and-genai-is-simply-the-next-evolution-9457132',
   },
   {
     imageUrl: '/Campaign-logo.png',
-    alt: 'News 2',
+    alt: 'News 3',
     bottomText: 'Carl Savio, founder and CEO of Bengaluru’s Artificial Labs feels that AI is an accelerator for ideas but that it cannot, will not in the foreseeable future.',
     bottomLink: 'https://www.campaignindia.in/article/ai-storm-will-agencies-sink-or-soar/504387',
   },
   {
     imageUrl: '/Curious-logo.png',
-    alt: 'News 3',
+    alt: 'News 4',
     bottomText: 'In Carl Savio’s AI short film, Rat City isn’t just another rodent block-it’s a full-blown gangster paradise. With a slick execution and a surprising twist.',
     bottomLink: 'https://curiousrefuge.com/ai-film-gallery/rat-city',
   },
